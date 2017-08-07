5a2b43a9544603e94fa93f0a7c0a2b06a54d727b
Dennis Quilantang <quildm@ymail.com>
2017-08-07 11:14:24 -0700

    created initial files

---
+++ src/com/codingdojo/web/controllers/Counter.java
@@ -0,0 +1,53 @@
+package com.codingdojo.web.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Counter
+ */
+@WebServlet("/Counter")
+public class Counter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Counter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Integer clicks = (Integer) session.getAttribute("clicks");
+		if (clicks == null) {
+			session.setAttribute("clicks", 0);
+		} else {
+			Integer clicks1 = (Integer) session.getAttribute("clicks");
+			clicks1++;
+			session.setAttribute("clicks", clicks1);
+		}
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
